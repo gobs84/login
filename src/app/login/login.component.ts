c19db400d753c882a058686b4030377003377566
Gabriel <gps9705@gmail.com>
2019-12-18 19:26:49 -0400

    Cleaning dependencies

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {MatSnackBar} from '@angular/material';
-import {LoginService} from '../loginService/login.service'
+import { MatSnackBar } from '@angular/material';
+import { LoginService } from '../loginService/login.service'
 import config from '../../assets/config.json'
 
 
@@ -11,8 +11,8 @@ import config from '../../assets/config.json'
 })
 export class LoginComponent implements OnInit {
 
-  private _configuration: { backEndUrl: any; postLogin: any; productsUrl:any}
-  constructor(private _snackBar: MatSnackBar, public _loginService: LoginService) { 
+  private _configuration: { backEndUrl: any; postLogin: any; productsUrl: any }
+  constructor(private _snackBar: MatSnackBar, public _loginService: LoginService) {
     this._configuration = config;
   }
 
@@ -22,22 +22,29 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
   snackbarMessage: string;
-  
 
-  login(){
-    this._loginService.login(this.username,this.password).then(response=>{
-      if(response==200){
-        this.snackbarMessage="You are logged";
-        window.location.href = this._configuration.productsUrl;
-      }
-      this._snackBar.open(this.snackbarMessage, "", {
+
+  login() {
+    if (this.username && this.password) {
+      this._loginService.login(this.username, this.password).then(response => {
+        if (response == 200) {
+          this.snackbarMessage = "You are logged";
+          window.location.href = this._configuration.productsUrl;
+        }
+        this._snackBar.open(this.snackbarMessage, "", {
+          duration: 3000,
+        });
+      }).catch(response => {
+        this._snackBar.open(response, "", {
+          duration: 3000,
+        });
+      });
+    } else {
+      this._snackBar.open("Por favor llene los campos", "", {
         duration: 3000,
-      }); 
-    }).catch(response=>{
-      this._snackBar.open(response, "", {
-        duration: 3000,
-      }); 
-    });
+      });
+    }
+    /**/
   }
 
 }
