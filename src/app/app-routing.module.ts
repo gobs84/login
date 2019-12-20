db5cf7a8ce78ac119bc49938ef0135601f41c0a3
Gabriel <gps9705@gmail.com>
2019-12-20 19:20:03 -0400

    release configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,8 @@ import {LoginComponent} from './login/login.component';
 
 
 const routes: Routes = [
-  { path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-  { path: 'login', component: LoginComponent },
+  
+  { path: '', component: LoginComponent },
   
 ];
 
